1a022e2b84cb116c7abcb92671ee97affeda5d02
SwipSwup <davidveigel.business@gmail.com>
2020-12-01 15:49:23 +0100

    added more osts to the seeder class

--- src/seeder.php
+++ src/seeder.php
@@ -3,11 +3,12 @@ include_once "ost.php";
 
 class seeder
 {
-    public static function createOsts(): array{
+    public static function createOsts(): array
+    {
         return [
             new ost(
                 1,
-                "Boneworks",
+                "Boneworks_OST",
                 "Boneworks",
                 "2019",
                 [
@@ -35,6 +36,70 @@ class seeder
                         "Michael Wyckoff",
                         294
                     ),
+                ]
+            ),
+            new ost(
+                2,
+                "Spider-Man_Miles_Morales_OST",
+                "Marvel's Spider-Man: Miles Morales",
+                "2020",
+                [
+                    new track(
+                        1,
+                        "Don't Give Up",
+                        "John Paesano",
+                        284
+                    ),
+                    new track(
+                        2,
+                        "Rhino Rampage",
+                        "John Paesano",
+                        279
+                    ),
+                    new track(
+                        3,
+                        "New York's Only Spider-Man",
+                        "John Paesano",
+                        207
+                    ),
+                    new track(
+                        4,
+                        "Spider-Training",
+                        "John Paesano",
+                        210
+                    )
+                ]
+            ),
+            new ost(
+                3,
+                "Titanfall_2_OST",
+                "Titanfall 2",
+                "2016",
+                [
+                    new track(
+                        1,
+                        "BT-7274",
+                        "Stephen Barton",
+                        337
+                    ),
+                    new track(
+                        2,
+                        "Rifleman Cooper",
+                        "Stephen Barton",
+                        159
+                    ),
+                    new track(
+                        3,
+                        "Apex Predators",
+                        "Stephen Barton",
+                        345
+                    ),
+                    new track(
+                        4,
+                        "Link to Pilot",
+                        "Stephen Barton",
+                        144
+                    )
                 ]
             )
         ];
