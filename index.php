cbbc523ba85d1d0e5a80f4ebb30f61d764791c26
SwipSwup <davidveigel.business@gmail.com>
2020-12-01 14:42:17 +0100

    added project structure

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BooTube</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="content">
+    <?php
+    require_once "php/Vimeo.php";
+    require_once "php/Youtube.php";
+    require_once "php/LocalVideo.php";
+
+
+    ?>
+</div>
+
+</body>
+</html>
+
