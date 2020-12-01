cb6c5da0525fa76d088a81aa6b8e6d963b1d1c98
AidsUnicorn <50658979+AidsUnicorn@users.noreply.github.com>
2020-12-01 15:24:05 +0100

    title changed

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>BooTube</title>
+    <title>OST for free</title>
     <link rel="stylesheet" href="css/style.css" type="text/css"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
 </head>
