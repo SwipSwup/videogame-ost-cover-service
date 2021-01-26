448b7dd13763aa94508e6bdfde6f4d8b030fd29a
SwipSwup <davidveigel.business@gmail.com>
2021-01-26 13:38:32 +0100

    Revert "Reformated and restructured project"
    This reverts commit 347cf9191284a214b5e348eaad3c19f3a08f67d1.

--- index.php
+++ index.php
@@ -3,23 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <title>videogame-ost-cover-service</title>
-    <link href="css/style.css" rel="stylesheet">
+    <link href="src/style.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-    <div class="cover">
-        <div class="transparent">
-            <h1>Suche OSTs</h1>
-            <ul>
-                <li>all: Liste aller OSTs</li>
-                <li>1-3: OST mit angegebener id</li>
-            </ul>
-            <form class="flex-form" method="get" action="src/HTTP/redirect.php">
-                <input type="text" name="request" placeholder="OST">
-                <button value="submit">Submit</button>
-            </form>
+    <div class="container">
+        <div class="cover">
+            <div class="transparent">
+                <h1>Suche OSTs</h1>
+                <ul>
+                    <li>all: Liste aller OSTs</li>
+                    <li>1-3: OST mit angegebener id</li>
+                </ul>
+                <form class="flex-form" method="get" action="redirect.php">
+                    <form action="">
+                        <input type="text" name="request" placeholder="OST">
+                        <button value="submit">Submit</button>
+                    </form>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 </body>
 </html>
--- request.php
+++ request.php
@@ -1,19 +1,20 @@
 <?php
-require_once "../class/Seeder.php";
+require_once "src/Seeder.php";
 
 header('Content-Type: application/json');
 
-if (isset($_GET['request'])) {
+if(isset($_GET['request'])) {
     $seeder = new Seeder();
     $osts = $seeder->createOsts();
 
-    if ($_GET['request'] === 'all') {
+    if($_GET['request'] === 'all') {
         echo json_encode($osts);
         exit();
-    } else {
+    }
+    else {
         $id = intval($_GET['request']);
-        if ($id != 0 && sizeof($osts) >= $id) {
-            $ost = $osts[$id - 1];
+        if($id!=0 && sizeof($osts)>=$id) {
+            $ost = $osts[$id-1];
             echo json_encode($ost);
             exit();
         }
